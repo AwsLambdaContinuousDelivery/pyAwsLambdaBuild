a976813f6ed017109d8c5d816040005104d39973
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2018-06-30 15:01:16 +0200

    Fix Typos and update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name='pyAwsLambdaBuild'
-     , version = '0.0.1'
+     , version = '0.0.2'
      , description = 'pyAwsLambdaBuild'
      , author = 'Janos Potecki'
      , url = 'https://github.com/AwsLambdaContinuousDelivery/pyAwsLambdaBuild'
